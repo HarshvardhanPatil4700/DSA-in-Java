01a4576dcb7fad0712584787303f99aa229f3a8a
harshpatil4700 <harshpatil4700@gmail.com>
2026-02-09 10:07:53 +0530

    Initial commit

---
+++ DSA/InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSort(int arr[],int n) {
+        for(int i = 0; i <= n-1; i++) {
+            int j = i;
+            while(j > 0 && arr[j-1] > arr[j]) { // j > 0 bcoz if j = 0 then,it has not arr[j-1] 
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+
+                j--; // to keep moving left until it reaches the correct position
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter length of array : ");
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of array : ");
+        for(int i=0;i< n;i++) {
+            arr[i] = s.nextInt();
+        }
+        
+        insertionSort(arr, n);
+
+        System.out.print("Sorted array : ");
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
