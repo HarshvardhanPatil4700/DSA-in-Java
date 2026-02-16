54f3e1eb644c9f478ddbbb9079c5e9cbdaf6327b
harshpatil4700 <harshpatil4700@gmail.com>
2026-02-16 10:35:32 +0530

    Second commit

--- DSA/Easy Questions/UnionAndIntersectionOfTwoSortedArrays.java
+++ DSA/Easy Questions/UnionAndIntersectionOfTwoSortedArrays.java
@@ -6,7 +6,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class UnionOfTwoSortedArrays {
+public class UnionAndIntersectionOfTwoSortedArrays {
     public static  List<Integer> uts(int a[],int b[]) {
         // 1. Brute force : TC = O(n1 logn + n2 logn) + O(n1+n2), SC = O(n1+n2) + O(n1+n2)
         // Set<Integer> unionSet = new HashSet<>();
@@ -25,8 +25,8 @@ public class UnionOfTwoSortedArrays {
         int n1 = a.length;
         int n2 = b.length;
 
-        int i = 0;
-        int j = 0;
+        int i = 0; // pointer in array a
+        int j = 0; // pointer in array b
         List<Integer> unions = new ArrayList<>();
         while(i < n1 && j < n2) {
             if(a[i] <= b[j]){
@@ -61,9 +61,55 @@ public class UnionOfTwoSortedArrays {
         return unions;
     }
 
+    public static List<Integer> its(int a[],int b[]) {
+        // 1. Brute force : TC = O(n1 * n2), SC = O(n2)
+        // int n1 = a.length;
+        // int n2 = b.length;
+        // int visited[] = new int[n2];
+        // List<Integer> intersections = new ArrayList<>();
+        // for (int i = 0; i < n1; i++) {
+        //     for(int j = 0; j < n2; j++) {
+        //         if(a[i] == b[j] && visited[j] == 0) {
+        //             intersections.add(a[i]);
+        //             visited[j] = 1;
+        //             break;
+        //         }
+        //         if(b[j] > a[i])
+        //             break;
+        //     }
+        // }
+        
+        // return intersections;
+
+        // 2. Optimal : 2 pointer approach - TC = O(n1+n2), SC = O(1)
+        int n1 = a.length; // pointer in array a
+        int n2 = b.length; // pointer in array b
+
+        List<Integer> intersections = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < n1 && j < n2) {
+            if(a[i] < b[j]) {
+                i++;
+            }
+            else if(b[j] < a[i]) {
+                j++;
+            } 
+            else {
+                intersections.add(a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return intersections;
+    }
     public static void main(String[] args) {
+        // 1. Union :
         int a[] = {1,1,2,3,4,5};
         int b[] = {2,3,4,4,5,6,7};
         System.out.println(uts(a, b));
+        // 2. Intersection :
+        System.out.println(its(a, b));
     }
 }
