98c12bb968cd6a01d1e14e0af62e1b5df702b8cd
harshpatil4700 <harshpatil4700@gmail.com>
2026-04-08 12:01:45 +0530

    Second commit

--- DSA/Medium Questions/LongestConsecutiveSequence.java
+++ DSA/Medium Questions/LongestConsecutiveSequence.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+// import java.util.ArrayList; for brute force
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
     // 1. Brute force : TC = O(n^2) SC = O(1)
@@ -38,15 +40,43 @@ public class LongestConsecutiveSequence {
     //     return false;
     // }
 
-    // 2. Optimal Sol : 
+    // 2. Optimal Sol : using order set ds
+    public static int longestSequence(int a[]) {
+        int n = a.length;
+        if (n == 0) return 0; // If the array is empty, no sequence exists
+        int longest = 1; // to store the length of longest sequence
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0;i<n;i++) { // inserting all array elements into set
+            set.add(a[i]);
+        }
+         
+        // Loop through each element in the set to find the starting point of consecutive sequences 
+        for (int it : set) {
+            if (!set.contains(it - 1)) { // If there is no number before 'it', it’s the start of a sequence
+                int cnt = 1; // Start the count for this sequence               
+                int x = it; // Store the current number
+
+                // Keep checking for the next consecutive number
+                while (set.contains(x + 1)) {
+                    // Move to the next number in sequence
+                    x = x + 1; 
+                    // Increment the length of current sequence
+                    cnt = cnt + 1; 
+                }
+
+                longest = Math.max(longest, cnt); // Update the longest sequence length if needed
+            }
+        }
+
+        return longest;
+    }
     
 
     public static void main(String[] args) {
         int a[] = {102,4,100,1,101,3,2,1,1};
-        int seq[] = longestSequence(a);
-
-        for(int n : seq) {
-            System.out.print(n + " ");
-        } 
+        int longestSeqLength = longestSequence(a);
+        System.out.println("The longest sequence length is : " + longestSeqLength);
     }
 }
