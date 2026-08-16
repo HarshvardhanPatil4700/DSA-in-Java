e7b84cd7490f461b204530cd2f0936e75e16caff
HarshvardhanPatil4700 <harshpatil4700@gmail.com>
2026-08-16 13:42:35 +0530

    5. Prefix sum

---
+++ DSA pattern wise/5.PrefixSum/PrefixSumIntro.java
@@ -0,0 +1,33 @@
+// Prefix Sum is a technique where we maintain the cumulative result of elements from the beginning of the array up to the current index. Core idea -> prefix[i] = prefix[i - 1] + arr[i]
+// Suffix Sum is the reverse of prefix sum. It stores the cumulative result of elements from the current index to the end of the array. Core idea -> suffix[i] = arr[i] + suffix[i + 1]
+
+import java.util.Arrays;
+
+public class PrefixSumIntro {
+    public static int[] prefixSum(int[] arr) {
+        int n = arr.length;
+        int prefix[] = new int[n];
+        for(int i=1;i<n;i++) {
+            prefix[i] = prefix[i-1] + arr[i-1];
+        }
+        return prefix;
+    }
+
+    public static int[] suffixSum(int[] arr) {
+        int n = arr.length;
+        int suffix[] = new int[n];
+        suffix[n-1] = 0;
+        for(int i=n-2;i>=0;i--) {
+            suffix[i] = arr[i+1] + suffix[i+1];
+        }
+        return suffix;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int[] prefSum = prefixSum(arr);
+        System.out.println(Arrays.toString(prefSum));
+        int[] suffSum = suffixSum(arr);
+        System.out.println(Arrays.toString(suffSum));
+    }
+}
